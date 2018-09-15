1e8fef201e314fd27936eccff9299676aceb266e
Adel Kovacs <kovacsadel12@gmail.com>
2018-09-15 21:46:57 +0200

    user loads on init

--- src/app/home/navigation.component.ts
+++ src/app/home/navigation.component.ts
@@ -2,11 +2,8 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {BreakpointObserver, Breakpoints, BreakpointState} from '@angular/cdk/layout';
 import {Observable} from 'rxjs';
 import {map, share} from 'rxjs/operators';
-import {ActivatedRoute} from "@angular/router";
 import {Subscription} from "rxjs/Subscription";
 import {WeddingService} from "../services/wedding.service";
-import {Wedding} from "../models/wedding.model";
-import {MapAdminComponent} from "./dashboard/cards-admin/map-admin/map-admin.component";
 import {MatDialog} from "@angular/material";
 import {NewWeddingDialogComponent} from "./new-wedding-dialog/new-wedding-dialog.component";
 
@@ -36,6 +33,7 @@ export class NavigationComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.myWeddings = this.weddingService.userUpdate.asObservable();
+    this.weddingService.getUser();
   }
 
   ngOnDestroy() {
